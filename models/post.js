2d11ea87aceb9d3bef7035dd9b3a576fb3a1b5d2
Milan <jmilan.97@gmail.com>
2022-04-17 22:48:30 +0200

    post model

---
+++ models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user: {type: Schema.Types.ObjectId, ref: "user", required: true},
+    text: {type: String, maxlength: 999},
+    image: {type: String},
+    comments: [{type: Schema.Types.ObjectId, ref: "comment"}],
+    likes: {type: Number, default: 0}
+});
+
+module.exports = mongoose.model("post", PostSchema);
