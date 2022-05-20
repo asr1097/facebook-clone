b1c47783cd70a221cf8c6513dda63bbd4f5c3c45
Milan <jmilan.97@gmail.com>
2022-05-20 12:33:34 +0200

    isLogged redirect auth

--- helpers.js
+++ helpers.js
@@ -1,5 +1,7 @@
 exports.isLogged = (req, res, next) => {
-    if(!req.user) {res.json({msg: "You are not loggeed in"})}
+    if(!req.user) {
+        res.redirect("/auth/facebook")
+    }
     next();
-}
+};
 
