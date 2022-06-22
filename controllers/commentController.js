e96ab84d548a5d062856ff13bef536f98f57d59f
Milan <jmilan.97@gmail.com>
2022-06-22 16:35:54 +0200

    post and comment edit and delete update

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,6 +3,27 @@ const Post = require("../models/post");
 const Comment = require("../models/comment");
 const { body, validationResult } = require("express-validator");
 
+const isSameUser = (req, res, next) => {
+	Comment.findById(req.body.commentID).then(comment => {
+		if(comment.user === req.user.id) {next()}
+		else{
+			console.log("Comment doesn't belong to logged user.")
+		}
+	})
+}
+
+const findChildComments = (parentCommentID) => {
+    Comment.find({parentComment: parentCommentID})
+    .then(comments => {
+        if(comments.length){
+            return comments;
+        } else {
+            return false;
+        }
+    })
+    .catch(err => console.log(err))
+}
+
 exports.getComment = (req, res, next) => {
     Comment.findById(req.params.id)
         .then(comment => {
@@ -34,7 +55,42 @@ exports.createComment = [
 ];
 
 exports.editComment = [
+    isSameUser,
 
+    body("text").isLength({max: 999}).trim().escape(),
+    body("postID").exists(),
+    body("commentID").exists(),
+
+    (req, res, next) => {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            res.json(err);
+        } else {
+            Comment.findByIdAndUpdate(req.body.commentID, {text: req.body.text})
+                .then(newComment => console.log("Comment edited."))
+                .catch(err => res.json(err));
+        };
+    }
+]
+
+exports.deleteComment = [
+    isSameUser,
+    
+    body("text").isLength({max: 999}).trim().escape(),
+    body("postID").exists(),
+    body("commentID").exists(),
+
+    (req, res, next) => {
+        let commentsToDelete = [req.body.commentID];
+        for(let i = 0; i < commentsToDelete.length; i++) {
+            let newCommentsToDelete = findChildComments(commentsToDelete[i]);
+            if(newCommentsToDelete) {commentsToDelete = [commentsToDelete, ...newCommentsToDelete]};
+            newCommentsToDelete = [];
+        };
+        Comment.deleteMany({_id: {$in: commentsToDelete}})
+            .then(deletedComments => console.log(deletedComments))
+            .catch(err => console.log(err));
+    }
 ]
 
 exports.likeComment = (req, res, next) => {
--- controllers/postController.js
+++ controllers/postController.js
@@ -3,6 +3,15 @@ const Post = require("../models/post");
 const Comment = require("../models/comment");
 const { body, validationResult } = require("express-validator");
 
+const isSameUser = (req, res, next) => {
+	Post.findById(req.body.postID).then(post => {
+		if(post.user === req.user.id) {next()}
+		else{
+			console.log("Post doesn't belong to logged user.")
+		}
+	})
+}
+
 exports.getPosts = (req, res, next) => {
     User.findById(req.user.id).then(loggedUser => {
         Post.find({$or: [{user: {$in: loggedUser.friendsList}}, {user: req.user.id}]})
@@ -41,6 +50,42 @@ exports.createPost = [
 		}
 	}
 ];
+
+exports.editPost = [
+	isSameUser,
+
+	body("text").isLength({max: 999}).trim().escape(),
+	body("postID").exists(),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if(!errors.isEmpty()) {
+			res.send("Text field must have maximum of 999 characters.")
+		} else {
+			Post.findByIdAndUpdate(req.body.postID, {text: req.body.text, image: req.body.image})
+				.then(editedPost => console.log("Post editted."))
+				.catch(err => res.json(err))	
+		};
+	}
+];
+
+exports.deletePost = [
+	isSameUser,
+	
+	body("postID").exists(),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if(!errors.isEmpty()) {
+			res.send("Post ID does not exist.")
+		} else {
+			Promise.all([
+				Comment.deleteMany({post: req.body.postID}),
+				Post.findByIdAndDelete(req.body.postID)
+			]).then(done => console.log("Post deleted")).catch(err => res.json(err))
+		}
+	}
+]
 
 exports.likePost = (req, res, next) => {
 	Post.findByIdAndUpdate(req.body.id, {$push: {likes: req.user.id}})
