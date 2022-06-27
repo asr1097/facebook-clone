7b6c9a6cccba11cce6abea637f7a7f9ccbb08d82
Milan <jmilan.97@gmail.com>
2022-06-27 23:29:05 +0200

    delete user update

--- routes/profile.js
+++ routes/profile.js
@@ -7,33 +7,36 @@ const helpers = require("../helpers");
 router.get("/:id", helpers.isLogged, userController.getUser);
 
 /* Send friend request */
-router.post("/:id/add", helpers.isLogged, userController.sendFriendRequest);
+router.post("/friends/:id/add", helpers.isLogged, userController.sendFriendRequest);
 router.get("/:id/add", helpers.isLogged, (req, res, next) => {
     res.render("sendFriendRequest")
 })
 
 /* Accept friend request */
-router.post("/:id/accept", helpers.isLogged, userController.acceptFriendRequest);
+router.post("/friends/:id/accept", helpers.isLogged, userController.acceptFriendRequest);
 router.get("/:id/accept", helpers.isLogged, (req, res, next) => {
     res.render("acceptRequest")
 })
 
 /* Reject friend request */
-router.post("/:id/reject", helpers.isLogged, userController.rejectFriendRequest);
+router.post("/friends/:id/reject", helpers.isLogged, userController.rejectFriendRequest);
 router.get("/:id/reject", helpers.isLogged, (req, res, next) => {
     res.render("rejectRequest")
 })
 
 /* Cancel friend request */
-router.post("/:id/cancel", helpers.isLogged, userController.cancelFriendRequest);
+router.post("/friends/:id/cancel", helpers.isLogged, userController.cancelFriendRequest);
 router.get("/:id/cancel", helpers.isLogged, (req, res, next) => {
     res.render("cancelRequest")
 })
 
 /* Remove friend */
-router.post("/:id/remove", helpers.isLogged, userController.removeFriend);
+router.post("/friends/:id/remove", helpers.isLogged, userController.removeFriend);
 router.get("/:id/remove", helpers.isLogged, (req, res, next) => {
     res.render("removeFriend")
 })
 
+/* Delete account */
+router.post("/:id/delete", helpers.isLogged, userController.deleteUser)
+router.get("/:id/delete", helpers.isLogged, (req, res) => res.render("deleteUser"))
 module.exports = router;
