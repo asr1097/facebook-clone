95a5df90b5bc2599d4edebff1297aff17da26589
Milan <jmilan.97@gmail.com>
2022-08-11 16:40:31 +0200

    cors reverse

--- app.js
+++ app.js
@@ -62,9 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieSession({
   name: "session",
   keys: [process.env.COOKIE_KEY],
-  maxAge: 24 * 60 * 60 * 1000,
-  sameSite: "none",
-  secure: true 
+  maxAge: 24 * 60 * 60 * 1000
 }))
 
 app.use(passport.initialize())
