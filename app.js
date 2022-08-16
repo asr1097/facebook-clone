d0548589adcadb9edda9ec89942962df17366d35
Milan <jmilan.97@gmail.com>
2022-08-16 11:34:52 +0200

    express static update 1

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(expres.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieSession({
   name: "session",
